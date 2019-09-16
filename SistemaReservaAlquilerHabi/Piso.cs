1f7b65b89353d91035d112c6de3bd69a0040f466
leanjpg <theleandrops3@gmail.com>
2019-09-15 22:04:37 -0400

    cambio de Leandro

---
+++ SistemaReservaAlquilerHabi/Piso.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaReservaAlquilerHabi
+{
+    class Piso
+    {
+        int nroPiso;
+        string descripcion;
+    }
+}
