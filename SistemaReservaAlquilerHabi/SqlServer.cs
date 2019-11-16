d1a4de57c1b25c9f1e69616d0642a45643f29a31
MariiiCuenca <mariiicuenca66@gmail.com>
2019-11-16 10:52:24 -0400

    agregue la clase SqlServer con la cadena de conexion respectiva

---
+++ SistemaReservaAlquilerHabi/SqlServer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace SistemaReservaAlquilerHabi
+{
+    public class SqlServer
+    {
+        public static string CADENA_CONEXION = ConfigurationManager.ConnectionStrings["SqlServer"].ConnectionString;
+
+    }
+}
